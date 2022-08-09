524d6224a2d0f35070adecae4e2b6ebb7b8f8dcd
INSPIRON3470S2\asters <e.oari@asters.co.jp>
2022-08-09 10:54:10 +0900

    Add：vueにtailwindcssを導入

--- docker/app/src/simple-board/src/views/AboutView.vue
+++ docker/app/src/simple-board/src/views/AboutView.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h4>Route: {{getThisRoute}}</h4>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </div>
 </template>
 
