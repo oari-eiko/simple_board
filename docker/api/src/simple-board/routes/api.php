9ff5c26ab7c351271e5276ca50520822aa0ccdc0
INSPIRON3470S2\asters <e.oari@asters.co.jp>
2022-08-23 19:33:44 +0900

    Fix：routeを微修正

--- docker/api/src/simple-board/routes/api.php
+++ docker/api/src/simple-board/routes/api.php
@@ -26,10 +26,6 @@ Route::controller(UserController::class)->group(function () {
     });
 });
 
-// Route::apiResources([
-//     'users' => UserController::class,
-// ]);
-
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
