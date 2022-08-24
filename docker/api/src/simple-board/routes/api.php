d5df273e8857ae891d82bee1cedf6226720eafb3
INSPIRON3470S2\asters <e.oari@asters.co.jp>
2022-08-24 18:35:52 +0900

    Add：全ての掲示板を取得するAPI追加

--- docker/api/src/simple-board/routes/api.php
+++ docker/api/src/simple-board/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,15 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Http\Controllers\UserController;
-
 // ユーザー関連
+use App\Http\Controllers\UserController;
 Route::controller(UserController::class)->group(function () {
     Route::prefix('users')->group(function () {
         Route::get('/', 'index');                   # 全ユーザー取得
         Route::get('/searchName', 'searchName');    # ユーザー名を検索
         Route::post('/', 'store');                  # ユーザー登録
         Route::get('/auth', 'auth');                # ユーザー認証
+    });
+});
+
+// 掲示板関連
+use App\Http\Controllers\BoardController;
+Route::controller(BoardController::class)->group(function () {
+    Route::prefix('board')->group(function () {
+        Route::get('/', 'index');                    # 全掲示板取得
     });
 });
 
