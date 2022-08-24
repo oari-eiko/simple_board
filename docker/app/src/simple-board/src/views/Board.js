2cba310d14b561da99b01e0df827928ec848d58a
INSPIRON3470S2\asters <e.oari@asters.co.jp>
2022-08-24 18:56:26 +0900

    Add：Apiで掲示板を取得して表示

--- docker/app/src/simple-board/src/views/Board.js
+++ docker/app/src/simple-board/src/views/Board.js
@@ -1,21 +1,38 @@
 // import { Link } from 'react-router-dom';
-// import { useState, useEffect } from 'react';
-// import axios from 'axios';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Board() {
   // state
-  
+  const [ boardItems, setBoardItems ] = useState([]);
 
   // effect
-  // useEffect(() => {
-  //   // 議題をAPIで取得
+  useEffect(() => {
+    // 全掲示板をAPIで取得
+    axios.get('http://localhost:8080/api/board/')
+    .then(res => {
+      let boardList = res.data.map(board => {
+        return (
+          <li key={board.id} className="p-3 border border-b-gray-300 bg-gray-100">
+            <div>
+              <p className='text-xl font-bold'>{board.title}</p>
+              <p className='text-sm'>{board.caption}</p>
+            </div>
+          </li>
+        );
+      });
+      setBoardItems(boardList);
+    })
+    .catch(error => console.log(error));
     
-  // }, []);
+  }, []);
 
   // JSX
   return (
-    <div className=''>
-      <p>Board Page</p>
+    <div className='flex flex-col'>
+      <ul className='mx-5 my-5'>
+        {boardItems}
+      </ul>
     </div>
   );
 }
